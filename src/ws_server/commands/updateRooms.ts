4afd35df9bd431058a188e826fa3852f9159ba87
NikFreeman <Classic76@tut.by>
2024-02-22 22:07:28 +0300

    refactor: updateRooms

--- src/ws_server/commands/updateRooms.ts
+++ src/ws_server/commands/updateRooms.ts
@@ -1,19 +1,19 @@
 import { MAX_USERS_IN_ROOM } from 'ws_server/models/consts';
-import { ResponseType } from 'ws_server/models/response';
 import { rooms } from 'ws_server/store';
 
-export function updateRoom() {
-  const data = rooms
-    .filter((item) => item.players.length < MAX_USERS_IN_ROOM)
-    .map((item) => {
-      return {
-        roomId: item.id,
-        roomUsers: item.players.map((elem) => {
-          return { name: elem.name, index: elem.id };
-        }),
-      };
-    });
+export function updateRooms() {
+  const responseData = JSON.stringify(
+    rooms
+      .filter((item) => item.players.length < MAX_USERS_IN_ROOM)
+      .map((item) => {
+        return {
+          roomId: item.id,
+          roomUsers: item.players.map((elem) => {
+            return { name: elem.name, index: elem.id };
+          }),
+        };
+      }),
+  );
 
-  const answer = JSON.stringify(data);
-  return { type: ResponseType.UPDATE_ROOM, data: answer, id: 0 };
+  return responseData;
 }
